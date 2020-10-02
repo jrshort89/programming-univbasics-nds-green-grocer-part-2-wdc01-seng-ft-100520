edc5faa72dd093c964b207f08ad9fa175f3323af
Jacob Short <jrshort89@gmail.com>
2020-10-02 00:56:18 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -24,7 +24,7 @@ def apply_clearance(cart)
   #
   # REMEMBER: This method **should** update cart
   cart.each { |item|
-    if item[:clearance]
+    if item[:clearance] == true
       item[:price] *= .8
     end
   }
